356b426fb9422f268066965f309629fa0d6f99e5
Faith Daisy Muchira <faidamuchira@gmail.com>
2026-04-03 11:59:39 +0100

    Add user input loop and basic program flow

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+while True:
+    # Ask user for a city
+    city = input("Please enter the name of the city: ")
+    print(city)
+    # Ask if they wish to add another city
+    choice = input(" Would you like to add another city? Y/N: ")
+    if choice == "N":
+        break
+
+    
+    
+    
+
